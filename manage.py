08502149dcfe5f03f88bcf07f69991a9f929e3fa
Alexander Paz <alexjpaz@gmail.com>
2019-01-13 10:07:34 -0500

    Use a class for Main

--- manage.py
+++ manage.py
@@ -1,7 +1,17 @@
 import os
 import sys
 
+class Main:
+
+    def __init__(self, args=[]):
+        pass
+
+    def run(self):
+        print(args)
+
+
 if __name__ == "__main__":
     args = sys.argv[1:]
 
-    print(args)
+    m = Main(args)
+    m.run()
